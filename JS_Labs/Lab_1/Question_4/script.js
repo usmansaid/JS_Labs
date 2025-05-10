bc4480680950992b5690433b19b49998d18c833a
Muhammad Usman Said <ussaid150@gmail.com>
2025-05-10 18:16:12 +0500

    Delete JS_Labs directory

--- JS_Labs/Lab_1/Question_4/script.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-let totalMinutes = prompt("Enter total minutes since midnight")
-
-let hours = Math.floor(totalMinutes / 60)
-let minutes = totalMinutes % 60
-
-console.log(hours + ":" + minutes);
-
